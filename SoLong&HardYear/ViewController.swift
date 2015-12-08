4aefa551429022f08e02dca1dd4c4301c01859f3
gemcave <stevendrosse@gmail.com>
2015-12-08 12:54:37 +0600

    Initial Commit

---
+++ SoLong&HardYear/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoLong&HardYear
+//
+//  Created by Zuck Rossental on 08.12.15.
+//  Copyright © 2015 gemcave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
